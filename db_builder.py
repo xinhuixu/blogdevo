f342b15353e81aeefd6605ac02785cd26e49e32e
nicolecheng <ncheng@stuy.edu>
2016-11-02 14:24:35 -0400

    update db_builder -- allocated fxns

--- db_builder.py
+++ db_builder.py
@@ -7,69 +7,81 @@ f="dumbbell.db"
 db = sqlite3.connect(f) #open if f exists, otherwise create
 c = db.cursor()    #facilitate db ops
 
+def go():
+    users()
+    all_story()
+    story()
+    close()
+
 #################################################################################################
-
-#make table the first
-q = "CREATE TABLE USERS (author TEXT, password TEXT)"
-c.execute(q)    #run SQL query
-
-#open csv
-fObj = open("users.csv")
-#read csv
-d=csv.DictReader(fObj)
-#add csv into table
-for k in d:
-    p = "INSERT INTO USERS VALUES('%s',%s)"%(k['author'],k['password'])
-    c.execute(p)
+def users():
+    #make table the first
+    q = "CREATE TABLE USERS (author TEXT, password TEXT)"
+    c.execute(q)    #run SQL query
+    
+    #open csv
+    fObj = open("users.csv")
+    #read csv
+    d=csv.DictReader(fObj)
+    #add csv into table
+    for k in d:
+        p = "INSERT INTO USERS VALUES('%s',%s)"%(k['author'],k['password'])
+        c.execute(p)
     
 
 ##################################################################################################
-
-#make table the second
-q = "CREATE TABLE ALL_STORY (story_id INTEGER, title TEXT, terminated BOOLEAN)"
-c.execute(q)
-
-#open csv the second
-fObj = open("all_story.csv")
-#read csv
-d = csv.DictReader(fObj)
-
-#add csv into table
-for k in d:
-    p = "INSERT INTO courses VALUES('%s',%s,%s)"%(k['story_id'],k['title'],k['terminated'])
-    c.execute(p)
+def all_story():
+    #make table the second
+    q = "CREATE TABLE ALL_STORY (story_id INTEGER, title TEXT, terminated BOOLEAN)"
+    c.execute(q)
+    
+    #open csv the second
+    fObj = open("all_story.csv")
+    #read csv
+    d = csv.DictReader(fObj)
+    
+    #add csv into table
+    for k in d:
+        p = "INSERT INTO courses VALUES('%s',%s,%s)"%(k['story_id'],k['title'],k['terminated'])
+        c.execute(p)
 
     
 ##################################################################################################
+def story():    
+    #make yet another table
+    q = "CREATE TABLE STORY (story_id INTEGER, author TEXT, content TEXT)"
+    c.execute(q)
     
-#make yet another table
-q = "CREATE TABLE STORY (story_id INTEGER, author TEXT, content TEXT)"
-c.execute(q)
-
-#open csv
-fObj = open("story.csv")
-#read csv
-d = csv.DictReader(fObj)
-
-#add csv into table
-for k in d:
-    p = "INSERT INTO courses VALUES('%s',%s,%s)"%(k['story_id'],k['author'],k['content'])
-    c.execute(p)
+    #open csv
+    fObj = open("story.csv")
+    #read csv
+    d = csv.DictReader(fObj)
+    
+    #add csv into table
+    for k in d:
+        p = "INSERT INTO courses VALUES('%s',%s,%s)"%(k['story_id'],k['author'],k['content'])
+        c.execute(p)
 
 ##################################################################################################
 
 #get wanted info
-q = "SELECT ALL_STORY.story_id, STORY.story_id FROM ALL_STORY,STORY WHERE ALL_STORY.story_id=STORY.story_id"
+#q = "SELECT ALL_STORY.story_id, STORY.story_id FROM ALL_STORY,STORY WHERE ALL_STORY.story_id=STORY.story_id"
 
-sel = c.execute(q)    #run SQL query
+#sel = c.execute(q)    #run SQL query
+#go()
+
+def get_prompts():
+    q = "SELECT story_id FROM ALL_STORY WHERE terminated=True"
+    # execute
+
+get_prompts()
 
 
 
-
-##################################################################################################    
-    
-db.commit() #save changes
-db.close()  #close database
+##################################################################################################
+def close():
+    db.commit() #save changes
+    db.close()  #close database
 
 
 ###########
