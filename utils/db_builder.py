668c8824d9768234cd1a12c43d951289de4ab09c
nicolecheng <ncheng@stuy.edu>
2016-11-03 14:47:10 -0400

    update

--- utils/db_builder.py
+++ utils/db_builder.py
@@ -5,62 +5,34 @@ import csv       #facilitates CSV I/O
 f="dumbbell.db"
 
 db = sqlite3.connect(f) #open if f exists, otherwise create
-c = db.cursor()    #facilitate db ops
-
-def go():
-    users()
-    all_story()
-    story()
-    close()
+#c = db.cursor()    #facilitate db ops
 
 #################################################################################################
 def users():
     #make table the first
-    q = "CREATE TABLE USERS (author TEXT, password TEXT)"
+    c = db.cursor()
+    q = "CREATE TABLE IF NOT EXISTS USERS (author TEXT, password TEXT)"
     c.execute(q)    #run SQL query
-    
-    #open csv
-    fObj = open("users.csv")
-    #read csv
-    d=csv.DictReader(fObj)
-    #add csv into table
-    for k in d:
-        p = "INSERT INTO USERS VALUES('%s',%s)"%(k['author'],k['password'])
-        c.execute(p)
+    #d = c.fetchall()
     
 
 ##################################################################################################
 def all_story():
     #make table the second
-    q = "CREATE TABLE ALL_STORY (story_id INTEGER, title TEXT, terminated BOOLEAN)"
+    c = db.cursor()
+    q = "CREATE TABLE IF NOT EXISTS ALL_STORY (story_id INTEGER, title TEXT, terminated BOOLEAN)"
     c.execute(q)
-    
-    #open csv the second
-    fObj = open("all_story.csv")
-    #read csv
-    d = csv.DictReader(fObj)
-    
-    #add csv into table
-    for k in d:
-        p = "INSERT INTO courses VALUES('%s',%s,%s)"%(k['story_id'],k['title'],k['terminated'])
-        c.execute(p)
+    #d = c.fetchall()
 
     
 ##################################################################################################
 def story():    
     #make yet another table
-    q = "CREATE TABLE STORY (story_id INTEGER, author TEXT, content TEXT)"
+    c = db.cursor()
+    q = "CREATE TABLE IF NOT EXISTS STORY (story_id INTEGER, author TEXT, content TEXT)"
     c.execute(q)
+    #d = c.fetchall()
     
-    #open csv
-    fObj = open("story.csv")
-    #read csv
-    d = csv.DictReader(fObj)
-    
-    #add csv into table
-    for k in d:
-        p = "INSERT INTO courses VALUES('%s',%s,%s)"%(k['story_id'],k['author'],k['content'])
-        c.execute(p)
 
 ##################################################################################################
 
@@ -70,21 +42,38 @@ def story():
 #sel = c.execute(q)    #run SQL query
 #go()
 
+def add_user(author,password):
+    c = db.cursor()
+    q = "INSERT INTO USERS VALUES("+author+","+password+")"
+    c.execute(q)
+
+def add_new_story(story_id,title,author,content):
+    terminated = 0
+    c = db.cursor()
+    q = "INSERT INTO ALL_STORY VALUES("+story_id+","+title+","+terminated+")"
+    c.execute(q)
+    c = db.cursor()
+    q = "INSERT INTO STORY VALUES('%s','%s','%s')"%(story_id,author,content)
+    c.execute(q)
+
+# 0 = false, 1 = true
 def get_prompts():
-    #q = "SELECT story_id FROM ALL_STORY WHERE terminated=1"
+    #q = "SELECT story_id FROM ALL_STORY WHERE terminated=0"
     #c.execute(q)
     c = db.cursor()
     q = "SELECT * FROM ALL_STORY"
     c.execute(q)
     d = c.fetchall()
     for n in d:
-        print n
-    print len(d)
+        return n
   
+#print get_prompts()
 
-get_prompts()
-
-
+def get_hash(username):
+    if check(username):
+        return USERS[username]
+    else:
+        return None
 
 ##################################################################################################
 def close():
@@ -92,7 +81,20 @@ def close():
     db.close()  #close database
 
 
+
+def go():
+    users()
+    all_story()
+    story()
+    close()
+
+go()
+
+
+
 ###########
 #TODO LIST
 # put everything into fxns
 # divide information pieces to feed into app.py
+# get_hash(username)
+# check(username) in db
