3cc42f4182d65307b63a49ee4b491cfad6b833c5
xinhuixu <x.xu.1102@gmail.com>
2016-11-05 21:48:17 -0400

    fix db path + login tmp

--- utils/db_builder.py
+++ utils/db_builder.py
@@ -3,7 +3,7 @@ import sqlite3   #enable control of an sqlite database
 import csv       #facilitates CSV I/O
 
 
-f="../data/dumbbell.db"
+f="data/dumbbell.db"
 
 db = sqlite3.connect(f) #open if f exists, otherwise create
 #c = db.cursor()    #facilitate db ops
