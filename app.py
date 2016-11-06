bcce25c2e5cda3b4eea5ebae57ee5aee68da2733
Shamaul Dilmohamed <Shamaul Dilmohamed>
2016-11-06 14:54:02 -0500

    auth function having issues with check

--- app.py
+++ app.py
@@ -26,40 +26,40 @@ def home():
 @app.route("/auth/", methods=["POST"])
 def auth():
     ## register
-    # if ("register" in request.form):
-        # if (request.form["username"] == "" or request.form["password"] == ""):
-            # flash("please fill in all forms of info")
-            # return redirect(url_for("login"))
-        # else if (db_builder.check(request.form["user"])):
-            # flash("username taken, please choose a new one")
-            # return redirect(url_for("login"))
-        # else:
-            # user0 = request.form["username"]
-            # pass = hash(request.form["password"])
-            # db_builder.add_user(user0,pass)
-            # flash("new account created")
-            # return redirect(url_for("login"))
+    print request.form
+    if "register" in request.form:
+        if (request.form["username"] == "" or request.form["password"] == ""):
+            flash("please fill in all forms of info")
+            return redirect(url_for("login"))
+        elif (db_builder.check(request.form["username"])):
+            flash("username taken, please choose a new one")
+            return redirect(url_for("login"))
+        else:
+            user0 = request.form["username"]
+            pass0 = request.form["password"]
+            db_builder.add_user(user0,pass0)
+            flash("new account created")
+            return redirect(url_for("login"))
     ## login
-    # else if ("login" in request.form)
-        # if (! db_builder.check(request.form['username'])):
-            # flash("username does not exist") 
-            # return redirect(url_for("login"))
-        # user1 = request.form['username']
-        # else if (! db_builder.getHash(user1) == (request.form['password'])):
-            # flash("incorrect username and password combination")
-            # return redirect(url_for("login"))
-            # remember to rehash pass
-        # else:
-            # session["user"] = user1
-    return redirect(url_for("home")) #remember to indent correctly
+    else:
+        if (not (db_builder.check(request.form['username']))):
+            flash("username does not exist") 
+            return redirect(url_for("login"))
+        elif (db_builder.getHash(user1) == request.form['password']):
+            user1 = request.form['username']
+            session["user"] = user1
+            return redirect(url_for("home"))
+        else:
+            flash("incorrect username and password combination")
+            return redirect(url_for("login"))
 
 @app.route("/logout/")
 def logout():
-    session.pop() #check which username actually logged out, or if its only one user at a time...
+    session.pop("user")
     return redirect(url_for("index"))
 
-def hash(password):
-    return hashlib.sha512(request.form['pass']).hexdigest()
+def hashp(password):
+    return hashlib.sha512(password).hexdigest()
 
 def add_content():
     return True
