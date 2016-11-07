b4a3bcf54cc2af394cd24005868a806a8fb6b6cc
Shamaul Dilmohamed <Shamaul Dilmohamed>
2016-11-07 03:02:46 -0500

    hashing done

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def auth():
             return redirect(url_for("login"))
         else:
             user0 = request.form["username"]
-            pass0 = request.form["password"]
+            pass0 = hashp(request.form["password"])
             db_builder.add_user(user0,pass0)
             flash("new account created")
             return redirect(url_for("login"))
@@ -52,7 +52,7 @@ def auth():
         if (not (db_builder.check(request.form['username']))):
             flash("username does not exist") 
             return redirect(url_for("login"))
-        elif (db_builder.get_hash(request.form['username']) == request.form['password']):
+        elif (db_builder.get_hash(request.form['username']) == hashp(request.form['password'])):
             user1 = request.form['username']
             session['username'] = user1
             print '!SESSION_STATUS: ' + session['username']
